efe5520415f0be59c6bc54d64c0042e654a0c065
bnjmnsmk <bnjmnsmk@gmail.com>
2025-06-29 16:57:39 +0200

    Final.2

--- app.py
+++ app.py
@@ -328,4 +328,4 @@ if __name__ == "__main__":
         return fig
     print("--- Callback made --- \n")
 
-    app.run(debug=True, host="127.0.0.1", port=8050)
+    app.run(debug=True, host="0.0.0.0", port=8050)
